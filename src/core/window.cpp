3083721f5fb504d509626c71360114a41ab4c473
phase <64127681+phasephasephase@users.noreply.github.com>
2025-06-08 13:57:45 -0600

    not a fluke

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -85,8 +85,7 @@ void window::finish_create(const HINSTANCE instance, const std::wstring& title, 
     ecs.add_component<drawable>(entity, quad, mart);
 
     auto& tr = ecs.add_component<transform>(entity);
-    tr.set_scale({ 1500, 1500 });
-    tr.set_rotation(30);
+    tr.set_scale({ 900, 550 });
 
     app.log.debug("Window systems initialized");
 }
