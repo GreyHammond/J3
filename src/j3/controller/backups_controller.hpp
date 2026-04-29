127f6f4918e658e9d2914b248e7067fa4265a468
phase <64127681+phasephasephase@users.noreply.github.com>
2026-04-29 11:44:59 -0600

    default backup path

--- src/j3/controller/backups_controller.hpp
+++ src/j3/controller/backups_controller.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "common.hpp"
+#include "framework/core/special_folder.hpp"
 
 #include "framework/ui/controller/controller.hpp"
 #include "j3/backup/backup_manager.hpp"
@@ -11,7 +12,7 @@ public:
     
 private:
     backups_model model;
-    backup_manager manager{ "./test_backups" };
+    backup_manager manager{ special_folder::get(FOLDERID_LocalAppData) / "J3" / "Backups" };
     
     void create_backup(Rml::DataModelHandle handle, Rml::Event& e, const Rml::VariantList& args);
     void apply_backup(Rml::DataModelHandle handle, Rml::Event& e, const Rml::VariantList& args);
